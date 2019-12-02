09d8be5a2192574c8caa5114b9ad51c68a03cbcf
cisnice <cisnice@naver.com>
2019-12-02 16:46:35 +0900

    test commit

--- startspringboot/src/main/java/com/cis/webservice/web/WebRestController.java
+++ startspringboot/src/main/java/com/cis/webservice/web/WebRestController.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebRestController {
     @GetMapping("/hello")
     public String hello() {
-        return "HelloWorld";
+        return "HelloWorld!!!";
     }}
